9f95881a5802e0024aa72a81a0b51384418f5010
ruthymng <ruth.ym.ng@gmail.com>
2017-02-15 13:43:22 +0000

    Looking at class names

--- 11getElsByClass.js
+++ 11getElsByClass.js
@@ -1,7 +1,29 @@
-function getElsByClass (s) {
+var jsdom = require('jsdom');
+var html = require('./testhtml.js');
 
+function getElsByClass (node, className) {
+    //for every child of node
+    //check to see if it's got the class className
+    //if it has, add it to an object
+    return node;   
 }
 
 
+//for every child of document, call getElsByClass. 
+
+
+
+
+
+    jsdom.env(
+        html,
+        function (err, window) {
+            console.log(
+            window.document.getElementsByClassName('a')[0].children
+            );
+        }
+    )
+
 
 module.exports = getElsByClass;
+
--- spec/11getElsByClass.spec.js
+++ spec/11getElsByClass.spec.js
@@ -2,25 +2,12 @@ var expect = require("chai").expect;
 var getElsByClass = require("../11getElsByClass.js");
 var jsdom = require('jsdom');
 
-// describe("getElsByClass()", function() {
-//   it("is a function", function() {
-//     expect(getElsByClass).to.be.a("function");
-//   });
-// //   it('works', function () {
-
-// //      var html =  jsdom.env(
-// //   "https://iojs.org/dist/",
-// //   ["http://code.jquery.com/jquery.js"],
-// //   function (err, window) {
-// //     console.log("there have been", window.$("a").length - 4, "io.js releases!");
-// //   }
-// // )
-
-//       expect(getElsByClass(html)).to.equal(html);
-//   });
-// });
-
-// console.log(
-
-
+describe("getElsByClass()", function() {
+  it("is a function", function() {
+    expect(getElsByClass).to.be.a("function");
+  });
+  it('works', function () {
+      expect(getElsByClass("html")).to.equal("html");
+  });
+});
 
